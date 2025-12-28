f288b68a4c2468c1bbecb3cd3fdfbc794da92778
苏俊杰 <momoyeyu@outlook.com>
2025-12-28 19:56:11 +0800

    demo code

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from fastapi-demo!")
+from fastapi import FastAPI
 
+# init FastAPI app
+app = FastAPI(
+    title="FastAPI + UV Project",
+    description="A FastAPI demo initialized by UV",
+    version="1.0.0"
+)
 
-if __name__ == "__main__":
-    main()
+# route
+@app.get("/")
+async def root():
+    return {"message": "Hello FastAPI + UV!"}
